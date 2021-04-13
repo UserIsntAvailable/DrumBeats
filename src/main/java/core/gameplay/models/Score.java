9ad96945ce5b1eafb7c2f75d68326081d5ee0842
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-04-12 23:57:45 -0400

    Add score model

---
+++ src/main/java/core/gameplay/models/Score.java
@@ -0,0 +1,25 @@
+﻿package core.gameplay.models;
+
+public class Score {
+	private final int _accuracy;
+	private final int _combo;
+	private final String _playerName;
+
+	public Score(int accuracy, int combo, String playerName) {
+		_accuracy = accuracy;
+		_combo = combo;
+		_playerName = playerName;
+	}
+
+	public int getAccuracy() {
+		return _accuracy;
+	}
+
+	public int getCombo() {
+		return _combo;
+	}
+
+	public String getPlayerName() {
+		return _playerName;
+	}
+}
