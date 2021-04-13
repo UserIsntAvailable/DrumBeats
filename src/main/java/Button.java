c4e00db7c074353e967ea4060b66fa8bf1f58a40
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-04-12 22:48:40 -0400

    Add base button class

---
+++ src/main/java/Button.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Button extends Actor {
+	private String _text;
+	private static GreenfootImage _image;
+
+	public Button(String text, GreenfootImage image) {
+		_text = text;
+		_image = image;
+	}
+
+	/**
+	 * Act - do whatever the Button wants to do. This method is called whenever
+	 * the 'Act' or 'Run' button gets pressed in the environment.
+	 */
+	public void act() {
+		
+	}
+}
