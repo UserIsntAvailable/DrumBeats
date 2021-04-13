bd9a9d40b011140ba3d2dadb3c1dfc4977d3f7e1
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-04-12 22:49:19 -0400

    Rename MyWorld -> Menu

---
+++ src/main/java/Menu.java
@@ -0,0 +1,11 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Menu extends World {
+	/**
+	 * Constructor for objects of class MyWorld.
+	 */
+	public Menu() {
+		// Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+		super(600, 400, 1);
+	}
+}
