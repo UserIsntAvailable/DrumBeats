60d29b16863860b8350342423aa23148dc203a0e
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-04-13 00:38:11 -0400

    Add PlayArea world

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,11 +1,8 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Menu extends World {
-	/**
-	 * Constructor for objects of class MyWorld.
-	 */
 	public Menu() {
-		// Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-		super(600, 400, 1);
+		// bounded = false because I will hide actors offscreen
+		super(1280, 720, 1, false);
 	}
 }
---
+++ src/main/java/PlayArea.java
@@ -0,0 +1,7 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class PlayArea extends World {
+	public PlayArea() {
+		super(1280, 720, 1);
+	}
+}
