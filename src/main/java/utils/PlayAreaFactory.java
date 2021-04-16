d52534a9548f8b3e11eac84c985a5aca24e631c3
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-04-16 14:19:16 -0400

    Add PlayAreaFactory class

---
+++ src/main/java/utils/PlayAreaFactory.java
@@ -0,0 +1,30 @@
+﻿package utils;
+
+import core.gameplay.models.Map;
+import core.graphics.Drawable;
+import greenfoot.Color;
+import greenfoot.World;
+
+public class PlayAreaFactory {
+	public PlayAreaFactory() {
+	}
+
+	public static World createPlayAreaWorldFromMap(Map map) {
+		
+		// TODO - If map is null create dummy map
+		
+		World playAreaWorld = new World(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT,
+				1, false) {
+		};
+		
+		playAreaWorld.setBackground(
+				Drawable.DrawRectangle(
+						Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT, Color.BLACK, "", null, null
+				)
+		);
+
+		// TODO - Parse the map, and the objects to the world
+
+		return playAreaWorld;
+	}
+}
