e253977176460c46e203f813f4bada2cba2382f6
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-04-15 22:36:05 -0400

    Change Drawable.drawRectangle method null checking

--- src/main/java/PlayArea.java
+++ src/main/java/PlayArea.java
@@ -1,9 +1,12 @@
+import core.graphics.Drawable;
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import utils.Constants;
 
 public class PlayArea extends World {
 	public PlayArea() {
 		super(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT, 1);
-		super.setBackground(new GreenfootImage("", 0, null, Color.BLACK));
+		super.setBackground(
+				Drawable.DrawRectangle(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT, Color.BLACK, "", null, null)
+		);
 	}
 }
--- src/main/java/core/graphics/Drawable.java
+++ src/main/java/core/graphics/Drawable.java
@@ -10,12 +10,12 @@ public class Drawable {
 		
 		// TODO - Have rounded rectangles
 		// TODO - Outline for the rectangle
-		
+
 		GreenfootImage baseImage = new GreenfootImage(width,height);
 		baseImage.setColor(background);
 		baseImage.fill();
-		baseImage.setColor(textColor);
-		baseImage.setFont(font);
+		baseImage.setColor(textColor == null ? Color.BLACK : textColor);
+		baseImage.setFont(font == null ? new Font(0) : font);
 		// BUG - This doesn't really place the string in the half of the button....
 		baseImage.drawString(text, width / 2, height / 2);
 
