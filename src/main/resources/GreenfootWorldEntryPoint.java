f2e41a717601773c17b1163bcfec153b5170bb5f
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-04-18 22:27:59 -0400

    Add standalone.porperties to create a JavaFX app later

---
+++ src/main/resources/GreenfootWorldEntryPoint.java
@@ -0,0 +1,18 @@
+import greenfoot.*;
+
+/**
+ * This is the entry point world for the greenfoot project, since I am force to provide a project.greenfoot
+ * as my request's. You can ignore this if you are running my code from an IDE ( but maybe I will use this
+ * to show the splashscreen too [since I am force to do one too...] ).
+ * 
+ * BTW, I know that I shouldn't write .java files on the resources folder, but this is the only way that I
+ * can include this class on the greenfoot project.
+ */
+public class GreenfootWorldEntryPoint extends World {
+
+	public GreenfootWorldEntryPoint() {
+		// I think that I can'
+		super(1280, 720, 1);
+		// TODO - Create a Startup class and call it from here.		
+	}
+}
