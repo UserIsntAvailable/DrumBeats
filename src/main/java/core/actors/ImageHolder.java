01af16aa8e8dc2832088d8e797de0d1f389b57c2
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-04-20 19:26:03 -0400

    Add ImageHolder class

---
+++ src/main/java/core/actors/ImageHolder.java
@@ -0,0 +1,10 @@
+package core.actors;
+
+import greenfoot.Actor;
+import greenfoot.GreenfootImage;
+
+public class ImageHolder extends Actor {
+	public ImageHolder(GreenfootImage image) {
+		this.setImage(image);
+	}
+}
