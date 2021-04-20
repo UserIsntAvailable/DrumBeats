8c46e7e2ca8900f9797fccf306a510665054dc4f
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-04-20 16:53:13 -0400

    ReAdd PlayField ( it would make more sense to have it as a class )

---
+++ src/main/java/core/worlds/PlayField.java
@@ -0,0 +1,19 @@
+﻿package core.worlds;
+
+import core.models.Map;
+import core.utils.Constants;
+import core.utils.WorldUtils;
+import greenfoot.Color;
+import greenfoot.World;
+
+public class PlayField extends World {
+	
+	public PlayField(Map map) {
+		super(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT, 1, true);
+
+		this.setBackground(WorldUtils.createWorldBackground(Color.BLACK));
+		
+		// TODO - If map is null create dummy playField
+		// TODO - Parse the map, and add the objects to this
+	}
+}
