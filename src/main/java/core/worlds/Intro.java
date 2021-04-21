cc20d09b3dfd55a30fd0e733b8b714c661fec46a
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-04-20 23:38:08 -0400

    Add GitImage class ( greenfoot imported class )

---
+++ src/main/java/core/worlds/Intro.java
@@ -0,0 +1,16 @@
+package core.worlds;
+
+import core.utils.Constants;
+import greenfoot.World;
+
+/**
+ * a.k.a SplashScreen
+ */
+public class Intro extends World {
+	public Intro() {
+		super(Constants.APP_WIDTH, Constants.APP_HEIGHT, 1);
+
+		// TODO - Create GIF animation
+		// TODO - Add "SplashScreen" animation ( I will use a GifImage )
+	}
+}
