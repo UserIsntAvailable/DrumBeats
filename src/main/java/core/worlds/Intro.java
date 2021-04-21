f19afef0be0824804a63612752c04d42738585f7
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-04-21 00:24:23 -0400

    Add Intro as the main World

--- src/main/java/core/worlds/Intro.java
+++ src/main/java/core/worlds/Intro.java
@@ -2,7 +2,6 @@ package core.worlds;
 
 import core.utils.Constants;
 import core.utils.GifImage;
-import core.utils.PathUtils;
 import greenfoot.Greenfoot;
 import greenfoot.World;
 
@@ -10,7 +9,7 @@ import greenfoot.World;
  * a.k.a SplashScreen
  */
 public class Intro extends World {
-	private final GifImage gifImage;
+//	private final GifImage gifImage;
 	
 	public Intro() {
 		super(Constants.APP_WIDTH, Constants.APP_HEIGHT, 1);
@@ -19,14 +18,12 @@ public class Intro extends World {
 		// TODO - Add "SplashScreen" animation ( I will use a GifImage )
 		
 		// This is just for testing...
-		gifImage = new GifImage(PathUtils.getFilePathFromResourcesFolder(Constants.MAIN_RESOURCES_PATH, "pretty-gif.gif"));
+		// FUCK GREENFOOT HE CAN SUCK MY DICK WITH THIS STUPID ERRORS
+//		gifImage = new GifImage("pretty-gif.gif");
 	}
 
 	@Override
 	public void act() {
-		if (gifImage.isRunning()) {
-		}
-		
 		if (Greenfoot.isKeyDown("space")) {
 			Greenfoot.setWorld(Menu.getInstance());
 		}
