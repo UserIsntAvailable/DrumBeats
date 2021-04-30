ec79090b8a1986f7c928d0b5871f26dd57587977
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-04-30 16:15:31 -0400

    Add PathUtils ( yeah, it came back )

---
+++ src/main/java/utils/PathUtils.java
@@ -0,0 +1,16 @@
+﻿package utils;
+
+import java.util.Objects;
+
+public class PathUtils {
+	public PathUtils() {
+	}
+
+	public static String getMapFromResources(String mapSetID, String mapID) {
+		return Objects.requireNonNull(
+				PathUtils.class
+						.getClassLoader()
+						.getResource("maps" + mapSetID + "/" + mapID))
+				.getPath();
+	}
+}
