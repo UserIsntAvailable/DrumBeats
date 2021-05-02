5e038a6a32c10f37577ccd4eed2d67948f8ba708
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-05-02 18:30:40 -0400

    Add AppUtils

---
+++ src/main/java/utils/AppUtils.java
@@ -0,0 +1,20 @@
+﻿package utils;
+
+import core.Config;
+
+public class AppUtils {
+	public AppUtils() {
+	}
+
+	/**
+	 * Get the delta time between the current frame and the last frame
+	 */
+	public static double getFramesDeltaTime(Config config) {
+		var currentTime = System.nanoTime();
+		var deltaTime = (currentTime - config.getValue(Long.class, "APP_LAST_FRAME_TIME")) / 1000000.0;
+		
+		config.setValue("APP_LAST_FRAME_TIME", currentTime);
+
+		return deltaTime;
+	}
+}
