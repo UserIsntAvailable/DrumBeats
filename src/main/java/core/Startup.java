a66592f45bfa47df03564f17ccaa437940439255
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-05-02 05:44:27 -0400

    Add drum key notes to the config startup values

--- src/main/java/core/Startup.java
+++ src/main/java/core/Startup.java
@@ -1,6 +1,10 @@
 package core;
 
+import enums.DrumType;
 import greenfoot.core.ImageCache;
+import javafx.util.Pair;
+
+import java.util.List;
 
 /**
  * Set up important things needed by the game like:
@@ -9,22 +13,31 @@ import greenfoot.core.ImageCache;
 public class Startup {
 	public Startup() {
 	}
-	
-	public static void setUpApplication(){
+
+	public static void setUpApplication() {
 		setUpConfig();
 		setUpImageCache();
 	}
 
 	private static void setUpImageCache() {
-		ImageCache imageCache = ImageCache.getInstance();
+		var imageCache = ImageCache.getInstance();
 
 		// Set up here the image cache here
 	}
-	
+
 	private static void setUpConfig() {
 		var config = Config.getInstance();
-		
+
 		config.setValue("APP_WIDTH", 1280);
 		config.setValue("APP_HEIGHT", 720);
+		config.setValue(
+				"APP_DRUM_KEYS",
+				List.of(
+						new Pair<>(DrumType.OUTER, "d"),
+						new Pair<>(DrumType.INNER, "f"),
+						new Pair<>(DrumType.INNER, "j"),
+						new Pair<>(DrumType.OUTER, "k")
+				)
+		);
 	}
 }
---
+++ src/main/java/handlers/PlayFieldUIHandler.java
@@ -0,0 +1,28 @@
+﻿package handlers;
+
+import core.Config;
+import greenfoot.World;
+
+/**
+ * This class handles all the UI objects ( Actors ) of a selected world
+ */
+public class UIHandler implements Handler {
+	private final World world;
+	private final Config config = Config.getInstance();
+	
+	public UIHandler(World world) {
+		this.world = world;
+	}
+
+	public void start() {
+		
+	}
+
+	public void configure() {
+		
+	}
+
+	public void close() {
+
+	}
+}
