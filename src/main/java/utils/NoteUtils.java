fe66dd14e44bf95a1d387b5316304a0cf5fe54c6
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-05-02 18:56:08 -0400

    Add NotesListUtils

---
+++ src/main/java/utils/NoteUtils.java
@@ -0,0 +1,12 @@
+﻿package utils;
+
+import models.NoteModel;
+
+public class NoteUtils {
+	public NoteUtils() {
+	}
+	
+	public static boolean isBigNote(NoteModel note) {
+		return note.getParams().split(",")[0].equals("1");
+	}
+}
---
+++ src/main/java/utils/NotesListUtils.java
@@ -0,0 +1,32 @@
+﻿package utils;
+
+import actors.NoteActor;
+import enums.TaikoNote;
+import models.NoteModel;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NotesListUtils {
+	public NotesListUtils() {
+	}
+
+	public static Collection<NoteActor> mapToNoteActor(List<NoteModel> noteModel, int diameter) {
+
+		return noteModel.stream()
+				.map(note -> {
+							if (note.getTaikoNote() != TaikoNote.SPINNER) {
+								if (NoteUtils.isBigNote(note)) {
+									return new NoteActor(diameter * 2, note);
+								}
+								return new NoteActor(diameter, note);
+
+							}
+							return null;
+						}
+				)
+				.collect(Collectors.toCollection(LinkedList::new));
+	}
+}
