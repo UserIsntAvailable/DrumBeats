3ba81c45faab8a1f69e99f2b7621c1a2274c26e5
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-05-10 07:46:08 -0400

    Fix gif intro

--- src/main/java/worlds/Intro.java
+++ src/main/java/worlds/Intro.java
@@ -8,47 +8,54 @@ import greenfoot.GreenfootImage;
 import greenfoot.World;
 import utils.GifImage;
 
+import java.util.List;
+
 /**
- * a.k.a SplashScreen. This is the "entry point" if the game is run from Greenfoot.
+ * a.k.a SplashScreen. This is the entry point of the game.
  */
 public class Intro extends World {
-	private final GifImage gifImage;
-	private final GreenfootImage firstFrame;
-	private final ImageHolder gifImageHolder;
-
+	//region Static Block	
 	static {
 		Startup.setUpApplication();
 	}
+	//endregion
+
+	//region Private Fields
+	private int currentFrame = 0;
+	private final ImageHolder gifImageHolder;
+	private final List<GreenfootImage> gifImages;
 
 	private static final Config config = Config.getInstance();
-
+	//endregion
+	
+	//region Constructor
 	public Intro() {
 		super(
 				config.getValue("APP_WIDTH"),
 				config.getValue("APP_HEIGHT"),
 				1
 		);
-		gifImage = new GifImage("splash-screen.gif");
-		firstFrame = gifImage.getCurrentImage();
-		gifImageHolder = new ImageHolder(firstFrame);
+		
+		var gifImage = new GifImage("splash-screen.gif");
+		gifImages = gifImage.getImages();
+		gifImageHolder = new ImageHolder(gifImage.getCurrentImage());
 		this.addObject(
 				gifImageHolder,
 				this.getWidth() / 2,
 				this.getHeight() / 2
 		);
 	}
+	//endregion
 
+	//region Public Methods
 	@Override
 	public void act() {
-		var gifFrame = gifImage.getCurrentImage();
-		if (gifFrame.equals(firstFrame) ||
-				Greenfoot.isKeyDown("space") ||
-				Greenfoot.mouseClicked(null)
-		) {
-
+		if (currentFrame >= gifImages.size() || Greenfoot.isKeyDown("space") || Greenfoot.mouseClicked(null)) {
 			Greenfoot.setWorld(Menu.getInstance());
 			return;
 		}
-		gifImageHolder.setImage(gifFrame);
+		gifImageHolder.setImage(gifImages.get(currentFrame));
+		currentFrame++;
 	}
+	//endregion
 }
